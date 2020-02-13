d212d20ad6c4a6f78f49b65ac456528b3ea120f2
SinhaAmrit <56241123+SinhaAmrit@users.noreply.github.com>
2020-02-13 06:34:45 +0530

    Update CircleArea.java

--- CircleArea.java
+++ CircleArea.java
@@ -5,15 +5,16 @@
 * Facebook : /codiizone
 * Instagram : @codiizone
 * wesite : https://codiizone.blogspot.com/
-* Q 4> Java Program to Accept Radius And Print The Area Of The Circle
+* Java Program to Accept Radius And Print The Area Of The Circle
 */
 import java.util.Scanner;
 //import java.lang.*;
 class CircleArea{
-	public static void main(String ...args){
-		System.out.println("Enter The Radius Of The Circle!");
-		Scanner kb=new Scanner(System.in);
-		int r=kb.nextInt();
-		System.out.println("Area Of The Circle Is "+((float)Math.PI*r*r)+" Units");
-	}
+    public static void main(String ...args){
+        System.out.println("Enter The Radius Of The Circle!");
+        Scanner kb=new Scanner(System.in);
+        int r=kb.nextInt();
+        System.out.println("Area Of The Circle Is "+((float)Math.PI*r*r)+" Units When Radius Is "+r+" Units");
+    }
 }
+Jo
