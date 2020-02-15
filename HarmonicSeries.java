ec2a2575babe4deabe74fa017c5b2589b04e69f7
SinhaAmrit <56241123+SinhaAmrit@users.noreply.github.com>
2020-02-15 19:18:55 +0530

    Create HarmonicSeries.java

---
+++ HarmonicSeries.java
@@ -0,0 +1,30 @@
+/**
+* @author : Amrit Sinha
+* Date : February 15,2020
+* GitHub : /SinhaAmrit
+* CodePen : /SinhaAmrit
+* Facebook : /codiizone
+* Instagram : /codiizone
+* website : https://codiizone.blogspot.com/
+* Java Program To Find Harmonic Series.
+*/
+import java.util.*;
+class HarmonicSeries
+{
+    public static void main(String args[])
+    {
+        int num, i = 1;
+        double rst = 0.0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number for length of series");
+        num = in.nextInt();
+        while (i <= num)
+        {
+            System.out.print("1/" + i + " +");
+            rst = rst + (double) 1 / i;
+
+            i++;
+        }
+        System.out.println("\n\nSum of Harmonic Series is " + rst);
+    }
+}
